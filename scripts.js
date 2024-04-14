35dc3cfd4c5515e2f8faa4bcb3626297c031372e
jheehsummer <jheeh.summer1927@gmail.com>
2024-04-14 11:22:44 -0300

    atualização "cidade não encontrada"

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 let chave = "d8922941defa5a3fd756f5d06b4f2d4a"
 
-function colocaNaTela (dados){
-console.log(dados)
+function colocaNaTela (dados) {
+    console.log(dados)
 
-document.querySelector(".mudar-cidade").innerHTML = dados.name
-document.querySelector(".graus-number").innerHTML = Math.floor (dados.main.temp) + "°C"
-document.querySelector(".descricao").innerHTML = dados.weather[0].description
-document.querySelector(".humidity").innerHTML = dados.main.humidity
-document.querySelector(".nuvem-sol").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-document.querySelector(".min").innerHTML = Math.floor (dados.main.temp_min) + "°"
-document.querySelector(".max").innerHTML = Math.floor (dados.main.temp_max) + "°"
+    if (dados.message === 'city not found') {
+        return document.querySelector(".cidade").innerHTML = "Cidade não encontrada!"
+    }
 
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".graus-number").innerHTML = Math.floor (dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".humidity").innerHTML = dados.main.humidity
+    document.querySelector(".nuvem-sol").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".min").innerHTML = Math.floor (dados.main.temp_min) + "°"
+    document.querySelector(".max").innerHTML = Math.floor (dados.main.temp_max) + "°"
 }
 
 async function buscarCidade(cidade) {
@@ -31,7 +34,6 @@ async function buscarCidade(cidade) {
 
 function cliqueiNoBotao() {
     let cidade = document.querySelector (".input-cidade").value
-
     buscarCidade(cidade)
-}
-
+   
+    }
